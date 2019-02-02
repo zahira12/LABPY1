7bd26dff11fa8b7d275b2757ebc16c6cd1ad3423
Zahraalfaiqoh <zahirazahraalfaiqoh28@gmail.com>
2019-02-02 11:23:55 +0700

    Menentukan Nilai Terbesar dari 3 Bilangan

---
+++ Labpy1.py
@@ -0,0 +1,14 @@
+a=input("nilai a 55")
+b=input("nilai b 44")
+c=input("nilai c 33")
+if a>b:
+    if a>c:
+        print(a)
+    else:
+        print(c)
+if b>c:
+    if b>c:
+        print(b)
+    else:
+        print(c)
+        
